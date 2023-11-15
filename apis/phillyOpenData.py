a5b4b419f2e47149b8fa011ea58e913bac15ac1c
jprichard <jprichard@gmail.com>
2023-11-14 23:51:35 -0500

    updated phillyOpenData api calls

--- apis/phillyOpenData.py
+++ apis/phillyOpenData.py
@@ -1,6 +1,41 @@
 # cartoApi
 import urllib.parse
 import requests
+import pandas
+import datetime
+
+def writeDateTimeFilter(
+          end_date
+        , interval
+        , base_query
+        , date_field
+):
+    # Create start date
+    start_date = (
+        (
+                pandas.to_datetime(
+                    datetime.datetime.strptime(end_date,"%m/%d/%Y")
+                ) - pandas.DateOffset(months = interval)
+        )
+         .to_period('m')
+         .strftime('%m/%d/%Y')
+    )
+
+    # truncate end_date
+    end_date = (
+        (
+                pandas.to_datetime(
+                    datetime.datetime.strptime(end_date,"%m/%d/%Y")
+                ) - pandas.DateOffset(months = 1)
+        )
+         .to_period('m')
+         .strftime('%m/%d/%Y')
+    )
+
+    # create datetime query
+    query = f"{base_query} WHERE {date_field} <= '{end_date}' AND {date_field} >= '{start_date}'"
+
+    return(query)
 
 
 class cartoApi:
@@ -15,3 +50,7 @@ class cartoApi:
     def queryRequest(self):
         request = requests.get(self.request_url)
         return request.content
+    
+    def queryDataframe(self):
+        data = pandas.read_csv(self.request_url)
+        return(data)
